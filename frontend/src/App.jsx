10e5f23f7d8a39143f3bc281ecaae33072e2df3c
Arun <arungagare2915@gmail.com>
2024-12-20 10:03:12 +0530

    here is the commit of frontend folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Form from './components/Form'
+
+import Table from './components/Table'
+import ViewDashBoard from './components/ViewDashBoard'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <Form/>
+    <div className='flex items-center justify-center'>
+      {/* <Form/> */}
+      {/* <Table/> */}
+      <ViewDashBoard/>
     </div>
   )
 }
